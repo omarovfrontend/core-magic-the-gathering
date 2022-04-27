389a7f0b0651196ecb066501746d02016a58770a
iGorillaz <barannik.denis.92@gmail.com>
2022-04-27 19:34:11 +0300

    Release 0 lotRouter

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const path = require('path');
+const createError = require('http-errors');
 require('dotenv').config();
+
+const cardRouter = require('.routes/card.router')
 const userRouter = require('./routes/user.router');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve(process.env.PWD, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/user', userRouter);
+app.use('/card', cardRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is started on port: ${PORT}`);
