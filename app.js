3d59bc40fd23d5eb45baf6fa0f99675efe058ff7
alextsyg <innempaya@gmail.com>
2022-04-27 19:49:58 +0300

    change user.router and app

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 require('dotenv').config();
 const userRouter = require('./routes/user.router');
-const PORT = process.env.PORT || 3002;
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve(process.env.PWD, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: 'secretword',
+    resave: false,
+    store: new FileStore(),
+    saveUninitialized: true,
+    name: 'mtg',
+    cookie: { httpOnly: true, maxAge: 60 * 60 * 1000 },
+  }),
+);
+
 app.use('/user', userRouter);
 
 app.listen(PORT, () => {
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,3 +1,59 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const { User } = require('../db/models');
+
+const saltRounds = 5;
+
+router
+  .route('/signup')
+  .get((req, res) => {
+    //res.render('signUp');
+    res.send('123');
+  })
+  .post(async (req, res) => {
+    const { name, email, password, city_id } = req.body;
+    const hashPass = bcrypt.hashSync(password, saltRounds);
+    const currentUser = await User.create({
+      nickname: name,
+      email: email,
+      password: hashPass,
+      city_id: city_id,
+    });
+    req.session.userId = currentUser.id; // для того, чтобы была сразу авторизация
+    req.session.lifestyle = currentUser.lifestyle;
+    res.redirect('/');
+  });
+
+// router
+//   .route('/signin')
+//   .get((req, res) => {
+//     res.render('signIn');
+//   })
+//   .post(async (req, res) => {
+//     const { nickName, password } = req.body;
+//     const user = await User.findOne({ where: { name: nickName } });
+//     const result = await bcrypt.compare(password, user.password); // сравниваем пароли
+//     if (result) {
+//       req.session.userId = user.id;
+//       req.session.lifestyle = user.lifestyle;
+//       res.redirect('/');
+//     } else {
+//       res.redirect('/user/signup');
+//     }
+//   });
+
+// router.get('/rap', rapCheck, (req, res) => {
+//   res.send('YA RONIAY ZAPAD');
+// });
+
+// router.get('/rock', rockCheck, (req, res) => {
+//   res.send('TZOY ZHIV');
+// });
+
+// router.get('/logout', (req, res) => {
+//   req.session.destroy();
+//   res.clearCookie('sid');
+//   res.redirect('/');
+// });
 
 module.exports = router;
