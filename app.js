6958a4cbdf6074d4073f9d0960e9769ea9685685
Artem <ha-chu@yandex.ru>
2022-04-27 23:46:10 +0700

    release hbs 0.1

---
+++ app.js
@@ -0,0 +1,32 @@
+// const express = require('express');
+
+// const path = require('path');
+// const app = express();
+
+// const PORT = 3000
+// const session = require('express-session');
+// const FileStore = require('session-file-store')(session);
+
+
+// const userRouter = require('./routers/user');
+// const staffRouter = require('./routers/staff');
+// const purchaseRouter = require('./routers/purchase');
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'hbs');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extends: true }));
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.get('/', (req, res) => {
+//     res.render('signin');
+// });
+
+// app.get('/', (req, res) => {
+//     res.render('signup');
+// });
+
+
+
+
+// app.listen(PORT, () => console.log(`Мы взломали икосаэдр на порту ${PORT}`))
