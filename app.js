89aa9c756c625a2a0455f975810032a0f4ffc1fd
Shamil Omarov <s_omarov05@mail.ru>
2022-04-28 12:30:31 +0300

    main.hbs - Added title

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 
 const app = express();
-// const path = require('path');
-
 const PORT = 3000;
 
 app.set('view engine', 'hbs');
-// app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.json());
