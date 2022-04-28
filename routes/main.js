48c8e3742732d35b1347a64a3f3ce94146edaa87
Shamil Omarov <s_omarov05@mail.ru>
2022-04-28 18:36:32 +0300

    Release good-job

--- routes/main.js
+++ routes/main.js
@@ -2,17 +2,8 @@ const router = require('express').Router();
 const { Card } = require('../db/models');
 
 router.route('/')
-.get(async (req, res) => {
-	let cards;
-	try {
-		cards = await Card.findAll({order:[['id', 'DESC']]});
-	} catch (error) {
-		return res.render('error', {
-			message: 'Не удалось загрузить карты',
-			error: {}
-		});
-	}
-return res.render('main', { cards });
+.get((req, res) => {
+ res.render('main');
 });
 
 
